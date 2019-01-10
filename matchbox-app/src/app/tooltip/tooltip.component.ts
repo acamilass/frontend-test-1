5ecff11a3a5af847f70f7bec6f424710ab77427f
Camila Santos <acamilass@gmail.com>
2019-01-10 20:36:38 -0300

    add rules to positive and negative rates

--- matchbox-app/src/app/tooltip/tooltip.component.ts
+++ matchbox-app/src/app/tooltip/tooltip.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tooltip',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TooltipComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+  arrayData: any [];
+  totalRate: number;
+  negativeRate: number [];
+  positiveRate: number [];
 
   ngOnInit() {
+    this.httpService.get('assets/matchboxbrasil.json').subscribe(data => {
+      this.arrayData = data['data'] as any [];
+
+    this.totalRate = this.arrayData.map(e => e.positive + e.negative).reduce((acumulador, atual) => {
+        return acumulador + atual;
+    });
+
+    this.negativeRate = this.arrayData.map(e => Math.round((e.negative * 100) / this.totalRate));
+
+    this.positiveRate = this.arrayData.map(e => Math.round((e.positive * 100) / this.totalRate));
+
+    console.log(this.negativeRate);
+    console.log(this.positiveRate);
+
+    },
+    (err: HttpErrorResponse) => {
+      console.log(err.message);
+    }
+  );
   }
 
 }
