12c2b67a1b03614b889140f63495ce57c987a616
Camila Santos <acamilass@gmail.com>
2019-01-09 21:00:41 -0300

    fix load json file

--- matchbox-app/src/app/card/card.component.ts
+++ matchbox-app/src/app/card/card.component.ts
@@ -13,9 +13,10 @@ export class CardComponent implements OnInit {
   arrayData: any [];
 
   ngOnInit() {
-    this.httpService.get('../../assets/matchboxbrasil.json').subscribe(data => {
-        this.arrayData = data as any [];
-        console.log(this.arrayData[1]);
+    this.httpService.get('assets/matchboxbrasil.json').subscribe(data => {
+        this.arrayData = data.data as any [];
+        // console.log(this.arrayData);
+        // console.log(data.data[0]);
       },
       (err: HttpErrorResponse) => {
         console.log(err.message);
